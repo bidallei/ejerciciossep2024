d20a0af4daeb41243650e76b991f4b1663245152
bidallei <hjalvaro@gmailcom>
2024-09-25 17:25:48 -0600

    Actualización prrimer programa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+Console.WriteLine("Por favor introduzca su nombre: ");
+
+Console.WriteLine("¿Cuántos alumnos hay en el salón?: ");
+
+//int numeroPersonas = 12;
+
+string nombrePersona = Console.ReadLine();
+
+string numeroPersonas = Console.ReadLine();
+
+Console.WriteLine("En este salón hay {0} personas y una se llama {1}", numeroPersonas, nombrePersona);
+
+Console.ReadKey();
