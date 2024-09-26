60e1b5bb389aacb3d30a1d9f195a6a61db50f655
bidallei <hjalvaro@gmailcom>
2024-09-25 18:44:27 -0600

    Ejercicios de clase 25 de septiembre

---
+++ ej_2024.09.25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please, enter the first number: ");
+
+int numberOne = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please, enter the second number: ");
+
+int numberTwo = int.Parse(Console.ReadLine());
+
+int operation = ((numberOne-numberTwo)*2);
+
+if (numberOne > numberTwo)
+{
+    Console.WriteLine("The first number is {0}. The second number is {1}. Since the first number is greater than the second, here you have the double of the difference: {2}", numberOne, numberTwo, operation);
+}
+else
+{
+    Console.WriteLine("The first number is not greater than the second.");
+}
---
+++ ej_2024.09.25_02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Until wich number do you want to know the odd numbers? ");
+
+int maximNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("The odd numbers from 1 to {0} are: ", maximNumber);
+
+for (int i = 1; i <= maximNumber; i+=2)
+{
+    Console.WriteLine(i);
+}
