bdba886821546c1e468bcf1614c6cbc44d5a0ed4
bidallei <hjalvaro@gmailcom>
2024-09-30 19:36:08 -0600

    Actualización de ejercicio hecho en clase

---
+++ ej_2024.09.30_01/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int[] arreglos = new int[5];
+
+arreglos[0] = 5;
+arreglos[1] = 1;
+arreglos[2] = 4;
+arreglos[3] = 2;
+arreglos[4] = 8;
+
+for(var y = 0; y < 4; y++)
+{
+    for(var x = 0; x < 4; x++)
+    {
+        if(arreglos[x]>arreglos[x+1])
+        {
+            int temporal = arreglos[x];
+            arreglos[x] = arreglos[x+1];
+            arreglos[x+1] = temporal;
+        }
+    }
+}
+
+for(var w = 0; w < 5; w++)
+{
+    Console.WriteLine(arreglos[w]);
+}
